56defd23af86630f316bb447e15be0599fb06319
marcos <marcgabe@myuax.com>
2024-12-04 17:25:51 +0100

    Create MyApplication.java
    Clase que he creado con el unico proposito de lanzar primero la vinculacion con firebase, para que no se vea solapado por otra tarea.

---
+++ app/src/main/java/com/example/marcosgb_parciall_2_1/MyApplication.java
@@ -0,0 +1,13 @@
+package com.example.marcosgb_parciall_2_1;
+
+import android.app.Application;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Call setPersistenceEnabled before any other Firebase operation
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
