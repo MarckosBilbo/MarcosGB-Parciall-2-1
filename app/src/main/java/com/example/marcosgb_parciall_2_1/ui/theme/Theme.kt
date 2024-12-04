976ec69da0b5fa7cbcbc4b39fabc6b18a9290457
marcos <marcgabe@myuax.com>
2024-12-04 17:27:05 +0100

    VISUAL
    Para ponerlo bonico y como dios manda.

--- app/src/main/java/com/example/marcosgb_parciall_2_1/ui/theme/Theme.kt
+++ app/src/main/java/com/example/marcosgb_parciall_2_1/ui/theme/Theme.kt
@@ -1,58 +1,36 @@
 package com.example.marcosgb_parciall_2_1.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = AzulPrimario,
+    onPrimary = TextoBlanco,
+    secondary = AzulClaro,
+    background = FondoClaro,
+    surface = AzulClaro,
+    onSurface = AzulOscuro
+)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = AzulClaro,
+    onPrimary = AzulOscuro,
+    secondary = AzulOscuro,
+    background = FondoOscuro,
+    surface = FondoOscuro,
+    onSurface = TextoBlanco
 )
 
 @Composable
-fun MarcosGBParciall21Theme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+fun MarcosGBParcial2_1Theme(
+    darkTheme: Boolean = false, // Cambiar a true para modo oscuro
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colors = if (darkTheme) DarkColorScheme else LightColorScheme
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+        colorScheme = colors,
+        typography = Typography, // Tipografías (opcional, definido abajo)
         content = content
     )
 }
